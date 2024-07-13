947a5305d74c33c44c11d79df8d6ba2e3526d3ad
江夏尧 <3446798488@qq.com>
2024-07-13 08:45:08 +0800

    feat: TOTP 服务器完成

--- totp-keeper/index.mjs
+++ totp-keeper/index.mjs
@@ -6,42 +6,64 @@ import * as OTPAuth from "https://esm.sh/otpauth";
 
 const app = new elysia();
 const kv = await Deno.openKv();
-
 /**
+ * 保存TOTP密钥
  */
 app.get("/save-secret", async (ctx) => {
-  const appName = ctx.query.appName;
-  const code = ctx.query.code;
-  if (!appName || !code) {
-    return new Response("AppName and code is required", { status: 400 });
-  }
+    const appName = ctx.query.appName;
+    const code = ctx.query.code;
+    if (!appName || !code) {
+        return new Response("AppName and code is required", { status: 400 });
+    }
 
-  await kv.set(["TOTP-secret-keeper", appName], code);
-  return { appName, code: 0 };
+    await kv.set(["TOTP-secret-keeper", appName], code);
+    return { appName, code: 0 };
 });
 
 /**
- * 生成 OTP
- * @todo 需要鉴权
+ * 生成一次性密码(OTP)
  */
 app.get("/generate-otp", async (req) => {
-  const appName = req.query.appName;
-  if (!appName) {
-    return new Response("Invalid appName", { status: 400 });
-  }
-  const secretKv = await kv.get(["TOTP-secret-keeper", appName]);
-  if (!secretKv.value)
-    return new Response(" User need to open TOTP first", { status: 400 });
+    const appName = req.query.appName;
+    if (!appName) {
+        return new Response("Invalid appName", { status: 400 });
+    }
+    const secretKv = await kv.get(["TOTP-secret-keeper", appName]);
+    if (!secretKv.value)
+        return new Response(" User need to open TOTP first", { status: 400 });
 
-  const secret = OTPAuth.Secret.fromBase32(secretKv.value);
-  const totp = new OTPAuth.TOTP({
-    secret: secret,
-    digits: 6,
-    period: 30,
-  });
+    const secret = OTPAuth.Secret.fromBase32(secretKv.value);
+    const totp = new OTPAuth.TOTP({
+        secret: secret,
+        digits: 6,
+        period: 30,
+    });
 
-  const token = totp.generate();
-  return { appName, otp: token };
+    const token = totp.generate();
+    return { appName, otp: token };
 });
 
+/**
+ * 验证一次性密码(OTP)
+ */
+app.get("/verify-otp", async (req) => {
+    const appName = req.query.appName;
+    const token = req.query.token;
+    if (!appName || !token) {
+        return new Response("Invalid request", { status: 400 });
+    }
+    const secretKv = await kv.get(["TOTP-secret-keeper", appName]);
+    if (!secretKv.value)
+        return new Response(" User need to open TOTP first", { status: 400 });
+
+    const secret = OTPAuth.Secret.fromBase32(secretKv.value);
+    const totp = new OTPAuth.TOTP({
+        secret: secret,
+        digits: 6,
+        period: 30,
+    });
+
+    const isValid = totp.validate({ token: token, window: 1 }) !== null;
+    return { appName, isValid };
+});
 Deno.serve(app.fetch);
--- totp-server/index.mjs
+++ totp-server/index.mjs
@@ -2,7 +2,7 @@
  * TOTP 服务就是常见的一次性 6 位验证码验证
  */
 import elysia from "https://esm.sh/elysia";
-import * as OTPAuth from "https://esm.sh/otpauth";
+import * as OTPAuth from "https://deno.land/x/otpauth/dist/otpauth.esm.js";
 
 const app = new elysia();
 const kv = await Deno.openKv();
@@ -12,14 +12,22 @@ const kv = await Deno.openKv();
  * @todo 需要鉴权
  */
 app.get("/generate-secret", async (ctx) => {
-  const username = ctx.query.username;
-  if (!username) {
-    return new Response("User ID is required", { status: 400 });
-  }
+    const username = ctx.query.username;
+    if (!username) {
+        return new Response("User ID is required", { status: 400 });
+    }
 
-  const secret = new OTPAuth.Secret();
-  await kv.set(["TOTP-secret", username], secret.base32);
-  return { username, secret: secret.base32, uri: totp.toString() };
+    const secret = new OTPAuth.Secret();
+    await kv.set(["TOTP-secret", username], secret.base32);
+    // Create a new TOTP object.
+    let totp = new OTPAuth.TOTP({
+        secret,
+    });
+    return {
+        username,
+        secret: secret.base32,
+        uri: OTPAuth.URI.stringify(totp),
+    };
 });
 
 /**
@@ -27,45 +35,45 @@ app.get("/generate-secret", async (ctx) => {
  * @todo 需要鉴权
  */
 app.get("/generate-otp", async (req) => {
-  const username = req.query.username;
-  if (!username) {
-    return new Response("Invalid User ID", { status: 400 });
-  }
-  const secretKv = await kv.get(["TOTP-secret", username]);
-  if (!secretKv.value)
-    return new Response(" User need to open TOTP first", { status: 400 });
+    const username = req.query.username;
+    if (!username) {
+        return new Response("Invalid User ID", { status: 400 });
+    }
+    const secretKv = await kv.get(["TOTP-secret", username]);
+    if (!secretKv.value)
+        return new Response(" User need to open TOTP first", { status: 400 });
 
-  const secret = OTPAuth.Secret.fromBase32(secretKv.value);
-  const totp = new OTPAuth.TOTP({
-    secret: secret,
-    digits: 6,
-    period: 30,
-  });
+    const secret = OTPAuth.Secret.fromBase32(secretKv.value);
+    const totp = new OTPAuth.TOTP({
+        secret: secret,
+        digits: 6,
+        period: 30,
+    });
 
-  const token = totp.generate();
-  return { username, otp: token };
+    const token = totp.generate();
+    return { username, otp: token };
 });
 
 // 验证 OTP
 app.get("/verify-otp", async (req) => {
-  const username = req.query.username;
-  const token = req.query.token;
-  if (!username || !token) {
-    return new Response("Invalid request", { status: 400 });
-  }
-  const secretKv = await kv.get(["TOTP-secret", username]);
-  if (!secretKv.value)
-    return new Response(" User need to open TOTP first", { status: 400 });
+    const username = req.query.username;
+    const token = req.query.token;
+    if (!username || !token) {
+        return new Response("Invalid request", { status: 400 });
+    }
+    const secretKv = await kv.get(["TOTP-secret", username]);
+    if (!secretKv.value)
+        return new Response(" User need to open TOTP first", { status: 400 });
 
-  const secret = OTPAuth.Secret.fromBase32(secretKv.value);
-  const totp = new OTPAuth.TOTP({
-    secret: secret,
-    digits: 6,
-    period: 30,
-  });
+    const secret = OTPAuth.Secret.fromBase32(secretKv.value);
+    const totp = new OTPAuth.TOTP({
+        secret: secret,
+        digits: 6,
+        period: 30,
+    });
 
-  const isValid = totp.validate({ token: token, window: 1 }) !== null;
-  return { username, isValid };
+    const isValid = totp.validate({ token: token, window: 1 }) !== null;
+    return { username, isValid };
 });
 
 Deno.serve(app.fetch);
