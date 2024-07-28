4272c7b350af0df1e79a87396e3dfa6b2f128b24
江夏尧 <3446798488@qq.com>
2024-07-28 22:29:23 +0800

    feat: 新增爬虫 fetch

---
+++ awesome-fetch/index.ts
@@ -0,0 +1,27 @@
+import makeFetchCookie from "fetch-cookie";
+import { randUserAgent } from "@tonyrl/rand-user-agent";
+
+export { randUserAgent };
+/**
+ * 提供爬虫专用的 fetch 函数，自动随机获取 user-agent
+ *  */
+export const AwesomeFetch = (fetch: typeof globalThis.fetch) => {
+    const fetchWithCookie = makeFetchCookie(fetch);
+    const config = {
+        agent: randUserAgent("desktop"),
+    };
+    const newFetch = (
+        input: RequestInfo | URL,
+        init: RequestInit = {}
+    ): Promise<Response> => {
+        if (typeof input === "string") {
+            input = new URL(input);
+        }
+        if (init.headers) {
+            init.headers = new Headers(init.headers);
+            init.headers.set("user-agent", config.agent);
+        }
+        return fetchWithCookie(input, init);
+    };
+    return { fetch: newFetch, config };
+};
