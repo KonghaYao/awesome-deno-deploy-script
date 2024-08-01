8907f30a99bc8ecda422b0e3108e090e3ffbcbad
3446798488 <3446798488@qq.com>
2024-08-01 07:45:10 +0000

    refactor: 重构 sono

--- chatroom/index.mjs
+++ chatroom/index.mjs
@@ -1,5 +1,17 @@
 import { Sono } from "jsr:@sono/core";
 const sono = new Sono();
-Deno.serve(async (req) => {
-    return sono.connect(req);
-});
+Deno.serve(
+  /**
+   * @param {Request} req
+   */
+  async (req) => {
+    const url = new URL(req.url);
+    if (url.pathname === "/ws") {
+      return sono.connect(req);
+    }
+    return new Response(
+      await Deno.readTextFile(new URL("./client.html", import.meta.url)),
+      { headers: { "content-type": "text/html" } }
+    );
+  }
+);
