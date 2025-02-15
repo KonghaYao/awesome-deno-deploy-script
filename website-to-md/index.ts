fe0e0edf271f1ce208e368d46010fdf71b92bdcf
江夏尧 <3446798488@qq.com>
2025-02-15 10:18:00 +0800

    feat: 添加 charset 解析

--- website-to-md/index.ts
+++ website-to-md/index.ts
@@ -2,6 +2,7 @@ import { z } from "https://deno.land/x/zod/mod.ts";
 import { Readability } from "https://esm.sh/@mozilla/readability";
 import TurndownService from "https://esm.sh/turndown";
 import { DOMParser } from "jsr:@b-fuze/deno-dom";
+import { decode, encode } from "https://esm.sh/iconv-lite";
 const schema = z.object({
     url: z.string().url(),
 });
@@ -19,17 +20,25 @@ async function handleRequest(req: Request): Promise<Response> {
         try {
             json = await req.json();
         } catch (_) {
-            return new Response(JSON.stringify({ error: "Invalid JSON" }), { status: 400 });
+            return new Response(JSON.stringify({ error: "Invalid JSON" }), {
+                status: 400,
+            });
         }
 
         if (!schema.safeParse(json).success) {
-            return new Response(JSON.stringify({ error: "Invalid URL" }), { status: 400 });
+            return new Response(JSON.stringify({ error: "Invalid URL" }), {
+                status: 400,
+            });
         }
 
         try {
             const res = await fetch(json.url);
-            const htmlText = await res.text();
-            const readableContent = (await extractReadableContent(htmlText)) ?? htmlText;
+            const charset = res.headers
+                .get("content-type")
+                ?.match(/charset=([^;]+)/)?.[1];
+            const htmlText = decodeCharset(await res.arrayBuffer(), charset);
+            const readableContent =
+                (await extractReadableContent(htmlText)) ?? htmlText;
 
             const turndownService = new TurndownService({
                 headingStyle: "atx",
@@ -40,10 +49,21 @@ async function handleRequest(req: Request): Promise<Response> {
 
             return new Response(markdown, { status: 200 });
         } catch (error) {
-            return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+            return new Response(JSON.stringify({ error: error.message }), {
+                status: 500,
+            });
         }
     } else {
         return new Response("Method not allowed", { status: 405 });
+    }
+}
+
+function decodeCharset(text: ArrayBuffer, charset: string = "utf-8") {
+    try {
+        return decode(new Uint8Array(text), charset);
+    } catch (error) {
+        console.error(`Failed to decode text with charset ${charset}:`, error);
+        return text;
     }
 }
 
